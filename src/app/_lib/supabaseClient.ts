1fb8706fa21cfebbb377db3df1e63d1c43d9b3f2
IlestChouette <allo@ilestchouette.fr>
2025-11-10 20:03:11 +0100

    Update supabaseClient.ts

--- src/app/_lib/supabaseClient.ts
+++ src/app/_lib/supabaseClient.ts
@@ -5,5 +5,11 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+{
+  auth: {
+    persistSession: false,
+      autoRefreshToken: false,
+      },
+      }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
